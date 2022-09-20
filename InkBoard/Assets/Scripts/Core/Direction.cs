3499f805e05d519a6277d77f4af9fc1d84629510
DustoTail <dustotail@gmail.com>
2022-09-20 20:04:51 +0900

    Added a list of all cardinal directions

--- InkBoard/Assets/Scripts/Core/Direction.cs
+++ InkBoard/Assets/Scripts/Core/Direction.cs
@@ -9,7 +9,7 @@ public class Direction
     ~Direction()
     {
     }
-
+    
     public static readonly Direction None = new Direction(Vector3Int.zero);
     public static readonly Direction Front = new Direction(Vector3Int.forward);
     public static readonly Direction Back = new Direction(Vector3Int.back);
@@ -17,7 +17,20 @@ public class Direction
     public static readonly Direction Right = new Direction(Vector3Int.right);
     public static readonly Direction Up = new Direction(Vector3Int.up);
     public static readonly Direction Down = new Direction(Vector3Int.down);
-
+    public static readonly Direction[] Directions = new Direction[]
+    {
+        None,
+        Front,
+        Back,
+        Left,
+        Right,
+        Up,
+        Down,
+    };
+    public static Direction GetDirection(int index)
+    {
+        return Directions[index];
+    }
     public static implicit operator Vector3Int(Direction dir) { return dir.m_Vector; }
 
     private Vector3Int m_Vector;
