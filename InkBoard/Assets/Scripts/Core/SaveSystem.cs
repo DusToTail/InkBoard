13ef1e922ba27df4c4bbca28dfee5a030e4d2293
DustoTail <dustotail@gmail.com>
2022-09-22 20:52:18 +0900

    Refactored SaveSystem (cleaner)

--- InkBoard/Assets/Scripts/Core/SaveSystem.cs
+++ InkBoard/Assets/Scripts/Core/SaveSystem.cs
@@ -3,26 +3,52 @@ using System.IO;
 
 public static class SaveSystem<T> where T : class, IPersistentData
 {
-    public static void Save(string filePath, T obj)
+    public static void Save(string filePath, T fromObj)
     {
         if (!File.Exists(filePath)) { File.Create(filePath); }
-        SaveData<T> data = new SaveData<T>(filePath, obj);
-        obj.SaveTo(data);
-        string content = JsonUtility.ToJson(data);
+        SaveObject saveObj = new SaveObject(filePath);
+        fromObj.SaveTo(saveObj.Data);
+        string content = JsonUtility.ToJson(saveObj.Data);
         File.WriteAllText(filePath, content);
     }
 
-    public static void Load(string filePath, T obj)
+    public static void Load(string filePath, T toObj)
     {
-        if (!File.Exists(filePath)) { Debug.LogWarning("FILE SYSTEM: " + filePath + " not found!"); obj = null; }
+        if (!File.Exists(filePath)) { Debug.LogWarning("FILE SYSTEM: " + filePath + " not found!"); toObj = null; }
         string content;
-        FileSystem<T>.Read(filePath, out content);
-        SaveData<T> data = JsonUtility.FromJson<SaveData<T>>(content);
-        obj.LoadFrom(data);
+        FileSystem.Read(filePath, out content);
+        SaveObject saveObj = new SaveObject(filePath);
+        saveObj.Data = JsonUtility.FromJson<T>(content);
+        toObj.LoadFrom(saveObj.Data);
+    }
+
+    private class SaveObject
+    {
+        public SaveObject(string filePath, T obj)
+        {
+            FilePath = filePath;
+            Data = obj;
+        }
+        public SaveObject(string filePath)
+        {
+            FilePath = filePath;
+            Data = null;
+        }
+        ~SaveObject()
+        {
+        }
+        public string FilePath { get; set; }
+        public T Data { get; set; }
     }
 }
 
-public static class FileSystem<T> where T : class, IPersistentData
+public interface IPersistentData
+{
+    public void LoadFrom(object data);
+    public void SaveTo(object data);
+}
+
+public static class FileSystem
 {
     public static void Write(string filePath, string content)
     {
@@ -35,22 +61,3 @@ public static class FileSystem<T> where T : class, IPersistentData
     }
 }
 
-public class SaveData<T> where T : class, IPersistentData
-{
-    public SaveData(string filePath, T obj)
-    {
-        FilePath = filePath;
-        Object = obj;
-    }
-    ~SaveData()
-    {
-    }
-    public string FilePath { get; set; }
-    public T Object { get; set; }
-}
-
-public interface IPersistentData
-{
-    public void LoadFrom(object data);
-    public void SaveTo(object data);
-}
