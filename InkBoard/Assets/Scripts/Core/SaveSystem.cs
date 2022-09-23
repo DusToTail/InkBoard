8cbe68b906523b794fe0eb0b7a68352f6f80c106
DustoTail <dustotail@gmail.com>
2022-09-24 01:31:52 +0900

    added the ability to append and read specific lines from file

--- InkBoard/Assets/Scripts/Core/SaveSystem.cs
+++ InkBoard/Assets/Scripts/Core/SaveSystem.cs
@@ -9,14 +9,30 @@ public static class SaveSystem<T> where T : class, IPersistentData
         SaveObject saveObj = new SaveObject(filePath);
         fromObj.SaveTo(saveObj.Data);
         string content = JsonUtility.ToJson(saveObj.Data);
-        File.WriteAllText(filePath, content);
+        FileSystem.WriteAll(filePath, content);
     }
-
+    public static void AppendAndSave(string filePath, T fromObj)
+    {
+        if (!File.Exists(filePath)) { File.Create(filePath); }
+        SaveObject saveObj = new SaveObject(filePath);
+        fromObj.SaveTo(saveObj.Data);
+        string content = JsonUtility.ToJson(saveObj.Data);
+        FileSystem.AppendText(filePath, content);
+    }
     public static void Load(string filePath, T toObj)
     {
         if (!File.Exists(filePath)) { Debug.LogWarning("FILE SYSTEM: " + filePath + " not found!"); toObj = null; }
         string content;
-        FileSystem.Read(filePath, out content);
+        FileSystem.ReadAll(filePath, out content);
+        SaveObject saveObj = new SaveObject(filePath);
+        saveObj.Data = JsonUtility.FromJson<T>(content);
+        toObj.LoadFrom(saveObj.Data);
+    }
+    public static void LoadLine(string filePath, T toObj, uint lineIndex)
+    {
+        if (!File.Exists(filePath)) { Debug.LogWarning("FILE SYSTEM: " + filePath + " not found!"); toObj = null; }
+        string content;
+        FileSystem.ReadLine(filePath, lineIndex, out content);
         SaveObject saveObj = new SaveObject(filePath);
         saveObj.Data = JsonUtility.FromJson<T>(content);
         toObj.LoadFrom(saveObj.Data);
@@ -50,14 +66,28 @@ public interface IPersistentData
 
 public static class FileSystem
 {
-    public static void Write(string filePath, string content)
+    public static void AppendText(string filePath, string content)
+    {
+        File.AppendAllText(filePath, content);
+    }
+    public static void WriteAll(string filePath, string content)
     {
         File.WriteAllText(filePath, content);
     }
 
-    public static void Read(string filePath, out string content)
+    public static void ReadAll(string filePath, out string content)
     {
         content = File.ReadAllText(filePath);
+    }
+    public static void ReadLines(string filePath, out string[] lines)
+    {
+        lines = File.ReadAllLines(filePath);
+    }
+    public static void ReadLine(string filePath, uint lineIndex, out string content)
+    {
+        var lines = File.ReadAllLines(filePath);
+        if(lineIndex >= lines.Length) { content = string.Empty; }
+        content = lines[lineIndex];
     }
 }
 
